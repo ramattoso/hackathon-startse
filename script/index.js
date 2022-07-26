ae8f41fbd48a54985a8079bbdfeaf6e2ecb07f7f
Luanftg <luanftgimenez@gmail.com>
2022-07-25 23:00:53 -0300

    testando loop para montagem do chart

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-import resultadosInvestimento from "./calculosInvestimentos";
+//import resultadosInvestimento from "./calculosInvestimentos";
 
 function valorFuturoDoAporteInicial(aporteInicial, taxaAoMes, prazoAoMes) {
   return aporteInicial * (1 + taxaAoMes / 100) ** prazoAoMes;
@@ -12,6 +12,37 @@ for (let i =1;i<=prazoAoMes;i++) {
   resultados.push(result);
 }
 
+console.log(resultados);
+
+switch (prazoAoMes) {
+  case 1: console.log('Janeiro')
+    break;
+  case 2: 'Fevereiro'
+    break;
+  case 3: 'Março'
+    break;
+  case 4: 'Abril'
+    break;
+  case 5: 'Maio'
+    break;
+  case 6: 'Junho'
+    break;
+  case 7: 'Julho'
+    break;
+  case 8: 'Agosto'
+    break;
+  case 9: 'Setembro'
+    break;
+  case 10: 'Outubro'
+    break;
+  case 11: 'Novembro'
+    break;
+  case 12: console.log('Dezembro');
+    break;
+
+}
+
+console.log(prazoAoMes);
 
 
 const labels = [
@@ -21,6 +52,11 @@ const labels = [
     'April',
     'May',
     'June',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Dezembro'
   ];
 
   const data = {
@@ -29,7 +65,7 @@ const labels = [
       label: 'My First dataset',
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45],
+      data: resultados,
     }]
   };
 
